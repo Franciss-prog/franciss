ca2a31c28b32d4972a9a9b30cb33d48521611245
francissprog <francissprog@gmail.com>
2025-08-30 20:16:37 +0800

    last-na

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -5,18 +5,18 @@
 </script>
 
 <footer
-	class={`flex h-20 flex-wrap items-center justify-between gap-4 border-t px-10 py-4 max-md:h-auto
-    ${$darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`}
+	class={`flex  h-20 w-full items-center justify-between gap-4 border-t px-10 
+py-4    ${$darkMode ? 'bg-dark text-light' : 'bg-light text-dark'}`}
 >
 	<!-- Logo -->
 	<a href="/" class="flex-shrink-0">
-		<img src={gif} alt="pfp" class="h-12" />
+		<img src={gif} alt="pfp" class="h-12 max-md:h-10 max-sm:h-8" />
 	</a>
 
 	<!-- Socials -->
-	<div class="flex gap-4 max-md:order-3 max-md:w-full max-md:justify-center">
+	<div class="max-sm:text-md flex gap-4 text-xl max-md:justify-center max-md:text-lg">
 		{#each socialMedia as { title, id, url } (id)}
-			<a href={url} aria-label="socmed" class="text-xl">
+			<a href={url} aria-label="socmed">
 				<i class={`bi bi-${title}`}></i>
 			</a>
 		{/each}
@@ -26,7 +26,7 @@
 	<button
 		aria-label="button"
 		on:click={() => scrollToHome('home')}
-		class={`flex rounded-full p-2 max-sm:order-2 max-sm:w-full max-sm:justify-center
+		class={`flex rounded-full p-2 max-md:hidden
         ${$darkMode ? 'bg-light text-dark' : 'bg-dark text-light'}`}
 	>
 		<i class="bi bi-arrow-up"></i>
